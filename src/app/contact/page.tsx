ee4e2dacc30ef9d79f3f59e4f228386cd3c60a11
celso <celso.sjunior@outlook.com>
2024-04-08 17:30:50 +0000

    ok

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -26,9 +26,9 @@ function ContactUs() {
           Contact Us
         </h1>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center">
-          We&apos;re here to help with any questions about AI,
+          We&apos;re here to help with any questions about our courses,
           programs, or events. Reach out and let us know how we can assist you
-          in your experience.
+          in your musical journey.
         </p>
         <form onSubmit={handleSubmit} className="space-y-4 mt-4">
           <input
