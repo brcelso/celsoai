649003c659f60acb90594e11bf1100dbf90b5031
celso <celso.sjunior@outlook.com>
2024-04-10 16:54:03 +0000

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ export default function LoginPage() {
   return (
     <main className="px-24">
       <Card className="rounded-2xl bg-slate-950 shadow-2xl text-white text-center h-[760px]">
-        <div className="mx-auto max-w-[683px] mt-[215px]">
-          <h1 className="text-6xl font-bold leading-tight">
-            Welcome to my page
-          </h1>
-          <p className="text-3xl mt-2">Please login to begin</p>
+        <div className="flex justify-center items-center h-full"> {/* Center content vertically and horizontally */}
+          <div className="mx-auto max-w-[683px]">
+            <h1 className="text-6xl font-bold leading-tight">
+              Welcome to my page
+            </h1>
+            <p className="text-3xl mt-2">Please login to begin</p>
+          </div>
         </div>
       </Card>
     </main>
