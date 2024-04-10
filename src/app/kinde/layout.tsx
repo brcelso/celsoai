177770cb6408933f7249e44a4665f225c253e1e6
celso <celso.sjunior@outlook.com>
2024-04-10 00:58:55 +0000

    ok

--- src/app/kinde/layout.tsx
+++ src/app/kinde/layout.tsx
@@ -22,44 +22,7 @@ import {
     return (
       <html lang="en">
         <body>
-          <header>
-            <nav className="nav container">
-              <h1 className="text-display-3">CelsoAuth</h1>
-              <div>
-                {!(await isAuthenticated()) ? (
-                  <>
-                    <LoginLink className="btn btn-ghost sign-in-btn">
-                      Sign in
-                    </LoginLink>
-                    <RegisterLink className="btn btn-dark">Sign up</RegisterLink>
-                  </>
-                ) : (
-                  <div className="profile-blob">
-                    {user?.picture ? (
-                      <img
-                        className="avatar"
-                        src={user?.picture}
-                        alt="user profile avatar"
-                        referrerPolicy="no-referrer"
-                      />
-                    ) : (
-                      <div className="avatar">
-                        {user?.given_name?.[0]}
-                        {user?.family_name?.[0]}
-                      </div>
-                    )}
-                    <div>
-                      <p className="text-heading-2">
-                        {user?.given_name} {user?.family_name}
-                      </p>
-  
-                      <LogoutLink className="text-subtle">Logout</LogoutLink>
-                    </div>
-                  </div>
-                )}
-              </div>
-            </nav>
-          </header>
+          
           <main>{children}</main>
           <footer className="footer">
             <div className="container">
