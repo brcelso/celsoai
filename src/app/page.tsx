d0293550bc0cab1ff395927e5d73a193ac2f5d7d
celso <celso.sjunior@outlook.com>
2024-04-10 17:05:41 +0000

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,14 @@ import { Card } from "@/components/ui/card";
 export default function LoginPage() {
   return (
     <main className="px-24">
-      <Card className="rounded-2xl bg-slate-950 shadow-2xl text-white text-center">
-        <div className="flex justify-center items-center"> {/* Center content vertically and horizontally */}
-          <div className="mx-auto max-w-[683px]">
-            <h1 className="text-6xl font-bold leading-tight">
-              Welcome to my page
-            </h1>
-            <p className="text-3xl mt-2">Please login to begin</p>
-          </div>
+      <Card className="rounded-2xl bg-slate-950 shadow-2xl text-white text-center h-[760px]">
+        <div className="mx-auto max-w-[683px] mt-[215px]">
+          <h1 className="text-6xl font-bold leading-tight">
+            Welcome to my page
+          </h1>
+          <p className="text-3xl mt-2">Please login to begin</p>
         </div>
       </Card>
     </main>
   );
 }
-
-
