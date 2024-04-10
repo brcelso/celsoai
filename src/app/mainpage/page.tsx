4a52dc20a4e4af8809ba01392741bc11d12ecfa3
celso <celso.sjunior@outlook.com>
2024-04-10 02:30:17 +0000

    ok

---
+++ src/app/mainpage/page.tsx
@@ -0,0 +1,25 @@
+
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import FirstHead from "@/components/FirstHead";
+
+
+export default function MainPage() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <FirstHead/>
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,20 @@
-
-import FeaturedCourses from "@/components/FeaturedCourses";
-import Footer from "@/components/Footer";
-import HeroSection from "@/components/HeroSection";
-import Instructors from "@/components/Instructors";
-import MusicSchoolTestimonials from "@/components/TestimonialCards";
-import UpcomingWebinars from "@/components/UpcomingWebinars";
-import WhyChooseUs from "@/components/WhyChooseUs";
-import FirstHead from "@/components/FirstHead";
-
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <FirstHead/>
-      <HeroSection />
-      <FeaturedCourses />
-      <WhyChooseUs />
-      <MusicSchoolTestimonials />
-      <UpcomingWebinars />
-      <Instructors />
-      <Footer />
+    <main className="px-24">
+      <Card className="rounded-2xl bg-slate-950 shadow-2xl text-white text-center h-[760px]">
+        <div className="mx-auto max-w-[683px] mt-[215px]">
+          <h1 className="text-6xl font-bold leading-tight">
+            Tools to support your online business
+          </h1>
+          <p className="text-3xl mt-2">Start your free trial today</p>
+          <Button className="mt-12" variant={"secondary"} size={"lg"}>
+            Sign up
+          </Button>
+        </div>
+      </Card>
     </main>
   );
 }
